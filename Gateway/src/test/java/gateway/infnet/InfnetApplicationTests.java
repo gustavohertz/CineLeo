a4a1468c7203c3f8aa667a2143518a23f7f119ea
Gustavo Hertz <gustavo.hertz@al.infnet.edu.br>
2026-06-09 14:00:03 -0300

    feat(gateway): configuração inicial do API Gateway e integração com Eureka
    - Correção das versões do Spring Boot (3.4.0) e Spring Cloud (2024.0.0) no pom.xml
    - Adição da dependência correta do spring-cloud-starter-gateway-mvc
    - Configuração do Eureka Client com exclusão do jersey3
    - Criação das rotas no application.properties para paymentService e microservicesKafka
    - Adição da anotação @EnableDiscoveryClient na classe principal

---
+++ Gateway/src/test/java/gateway/infnet/InfnetApplicationTests.java
@@ -0,0 +1,13 @@
+package gateway.infnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
