43d5286098b87b7723cd148d80aa1c095abf7fc8
Giovani Sanfer <giovani.marlonfer@gmail.com>
2026-06-10 21:22:57 -0300

    feature: corrige config de payment

---
+++ Notification/src/main/java/com/leocine/notificationApp.java
@@ -0,0 +1,13 @@
+package com.leocine;
+
+/**
+ * Hello world!
+ *
+ */
+public class notificationApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
