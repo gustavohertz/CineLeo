3376e529565e96f1bb6b69ccd56f1aa8c25b8b47
Giovani Sanfer <giovani.marlonfer@gmail.com>
2026-06-21 01:43:58 -0300

    feature: fix kafka

---
+++ Kafka/src/main/java/com/infnet/microservicesKafka/dto/ConsumeResponseDTO.java
@@ -0,0 +1,3 @@
+package com.infnet.microservicesKafka.dto;
+
+public record ConsumeResponseDTO(String id, String status) {}
--- Kafka/src/test/java/com/infnet/microservicesKafka/MicroservicesKafkaApplicationTests.java
+++ Kafka/src/test/java/com/infnet/microservicesKafka/MicroservicesKafkaApplicationTests.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(properties = "eureka.client.enabled=false")
 class MicroservicesKafkaApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
